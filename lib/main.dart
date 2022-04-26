940f875def96babd7b2deabfaa5df3f5ada62f1b
DevBees <82749068+DevBees@users.noreply.github.com>
2022-04-27 01:51:59 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
-import 'package:calculator/calcUI.dart';
 import 'package:flutter/material.dart';
 
+import 'calcUi.dart';
+
 void main() {
-  runApp(MaterialApp(
-    home: CalcUI(),
-    theme: ThemeData(
-      primaryColor: Colors.white,
-      accentColor: Colors.black,
+  runApp(
+    MaterialApp(
+      home: CalcUI(),
+      theme: ThemeData(
+        primaryColor: Colors.white,
+        accentColor: Colors.black,
+      ),
+      debugShowCheckedModeBanner: false,
     ),
-    debugShowCheckedModeBanner: false,
-  ));
+  );
 }
